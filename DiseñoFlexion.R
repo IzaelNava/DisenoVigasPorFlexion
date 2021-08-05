985148083017dda91c79065dd9e7206031e9dd79
izaelNava <inavav@imp.mx>
2021-08-05 10:45:36 -0500

    Nuevo script

---
+++ DiseñoFlexion.R
@@ -0,0 +1,16 @@
+# Datos de entrada
+
+b            <- 30    # cm
+pef          <- 52    # cm
+recub        <- 3     # cm
+fc           <- 250   # kg/cm2
+fy           <- 4200  # kg/cm2
+ecu          <- 0.003 # 
+Mu           <- 25    # ton-m
+FR           <- 0.9   # 
+costConcreto <- 1400  # m3
+costAcero    <- 18    # kg
+
+# Cálculos
+
+B1           <- 0.85  
